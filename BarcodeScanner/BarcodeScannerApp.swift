b14faa3a3e84d806e8cd74a21b8ce7d83e303b3d
Engin Bolat <engi.bolat@gmail.com>
2024-06-20 12:10:57 +0300

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Engin Bolat on 20.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
